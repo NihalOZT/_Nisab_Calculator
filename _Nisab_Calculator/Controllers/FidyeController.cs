510f4c36b5ed4c5b39ef298b962150681b6281eb
Nihal ÖZTÜRK <nihalozturk.iletisim@gmail.com>
2024-02-09 01:58:53 +0300

    final commit

--- _Nisab_Calculator/Controllers/FidyeController.cs
+++ _Nisab_Calculator/Controllers/FidyeController.cs
@@ -4,29 +4,30 @@ using _Nisab_Calculator.Models;
 
 namespace _Nisab_Calculator.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/[controller]")]
     public class FidyeController : Controller
     {
+        
         [HttpGet]
         [Route("getBakliyatData")]
-        public FidyeCalculatorDto getBakliyatFiyatları()
+        public FidyeCalculatorDto getBakliyatFiyatları([FromBody] FidyeCalculatorDto fidyeCalculator)
         {
-            FidyeCalculatorDto fidyeCalculator = new FidyeCalculatorDto();
             fidyeCalculator.ArpaFiyatı = 60 * 3.500 * 30 * fidyeCalculator.AileÜyesi;
             fidyeCalculator.UnFiyatı = 70 * 1.750 * 30 * fidyeCalculator.AileÜyesi;
             fidyeCalculator.HurmaFiyatı = 300 * 3.500 * 30 * fidyeCalculator.AileÜyesi;
             fidyeCalculator.KuruÜzümFiyatı = 80 * 3.500 * 30 * fidyeCalculator.AileÜyesi;
             fidyeCalculator.BuğdayFiyatı = 40 * 1.750 * 30 * fidyeCalculator.AileÜyesi;
 
+            
             return fidyeCalculator;
         }
         [HttpPost]
         [Route("calculateFidye")]
         public IActionResult Index([FromForm] FidyeCalculatorDto fidyeCalculator)
         {
-            FidyeCalculatorDto calculateFidye = getBakliyatFiyatları();
+            FidyeCalculatorDto calculateFidye = getBakliyatFiyatları(fidyeCalculator);
 
-            return View("Index", calculateFidye);
+            return View("./Views/Fidye/Index.cshtml", calculateFidye);
 
         }
         
--- _Nisab_Calculator/Program.cs
+++ _Nisab_Calculator/Program.cs
@@ -1,7 +1,9 @@
+using _Nisab_Calculator.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews();;
 
 var app = builder.Build();
 
@@ -22,7 +24,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "fidye",
-    pattern: "api/{controller=Fidye}/{action=Index}/{id?}");
+    pattern: "{controller=Fidye}/{action=Index}/{id?}");
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
